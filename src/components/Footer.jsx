5f816143bd3070ad6541c49f2e399238bbe459f9
Adegbite Ademola kelvin <Adegbiteademola1999@gmail.com>
2021-12-24 23:44:10 +0100

    chore: fix styliing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
                 <button><a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_self_edit_contact_info%3BIXets8AGSymmVPMt4c1Ahg%3D%3D"><BsLinkedin size={20}/></a></button>
                 <button><a href="https://api.whatsapp.com/send?phone=8104750773&text=Hello%20kelvin"><BsWhatsapp size={20}/></a></button>
             </div>
-            <hr/>
+            <hr className="hr"/>
             <div className="right">
                 <p>All rights reserved. (C) 2020</p>
                 <p>Designed and developed by <a className='link-dev' href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_self_edit_contact_info%3BIXets8AGSymmVPMt4c1Ahg%3D%3D">Ademola Kelvin</a></p>
